e2b4cdd80b52c8802bc67f54c636d7cdd6d8c963
GermanAliaga <usami.haruka01@gmail.com>
2024-11-06 19:34:24 -0300

    barista

--- lib/models/mibarista.dart
+++ lib/models/mibarista.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:homecoffe/models/comentario.dart';
 import 'package:homecoffe/models/receta.dart';
 
-class Person extends ChangeNotifier {
+class Barista extends ChangeNotifier {
   late String nombre = "Barista";
   var listaRecetas = <Receta>{};
   var listaFavoritos = <Receta>{};
