ce7d0fcb307b5ffeb3c2af124b56280fcdb90883
GermanAliaga <usami.haruka01@gmail.com>
2024-11-07 10:04:03 -0300

    arreglo

--- lib/pages/splashscreen.dart
+++ lib/pages/splashscreen.dart
@@ -12,7 +12,7 @@ class Splashscreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<Splashscreen> {
-  String logo = "assets/icons/Logo.svg";
+  String logo = "assets/icons/Logo2.svg";
   Timer? _timer;
 
   @override
