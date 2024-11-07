a66c1dd1531968dfcfa9e52ee36e8fb880f408c3
GermanAliaga <usami.haruka01@gmail.com>
2024-11-07 09:18:19 -0300

    json

--- lib/pages/splashscreen.dart
+++ lib/pages/splashscreen.dart
@@ -12,7 +12,7 @@ class Splashscreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<Splashscreen> {
-  String coffeeMachine = "assets/icons/3098737_coffee_machine2.svg";
+  String logo = "assets/icons/Logo.svg";
   Timer? _timer;
 
   @override
@@ -45,7 +45,7 @@ class _SplashScreenState extends State<Splashscreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           SvgPicture.asset(
-            coffeeMachine,
+            logo,
             semanticsLabel: 'Logo',
             width: 200,
           )
@@ -54,65 +54,3 @@ class _SplashScreenState extends State<Splashscreen> {
     ));
   }
 }
-
-
-
-/*import 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:homecoffe/pages/iniciosesion.dart';
-
-class Splashscreen extends StatefulWidget {
-  const Splashscreen({super.key});
-
-  @override
-  State<Splashscreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<Splashscreen>
-    with SingleTickerProviderStateMixin {
-  @override
-  void initState() {
-    super.initState();
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-    Future.delayed(
-        const Duration(seconds: 3),
-        {
-          Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (_) => const Iniciosesion()))
-        } as FutureOr Function()?);
-  }
-
-  @override
-  void dispose() {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-        overlays: SystemUiOverlay.values);
-  }
-
-  String coffeeMachine = "assets/icons/3098737_coffee_machine2.svg";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      width: double.infinity,
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [
-        Color.fromARGB(248, 255, 206, 160),
-        Color.fromARGB(255, 153, 67, 1),
-      ], begin: Alignment.topRight, end: Alignment.bottomLeft)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SvgPicture.asset(
-            coffeeMachine,
-            semanticsLabel: 'Logo',
-            width: 90,
-          )
-        ],
-      ),
-    ));
-  }
-}*/
--- lib/pages/tuopinion.dart
+++ lib/pages/tuopinion.dart
@@ -23,12 +23,6 @@ class _TuOpinionState extends State<TuOpinion> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                const TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: '...',
-                  ),
-                ),
                 ElevatedButton(
                     onPressed: () {
                       Navigator.pop(context);
